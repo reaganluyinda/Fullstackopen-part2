5cc7b7bec537aadc584b1e977404a7134ed62b6c
reagan luyinda <luyindareagan@gmail.com>
2025-11-02 18:18:11 +0300

    Phonebook (2.6) step 1, add new person to phone book

--- Phonebook/phonebook/src/App.jsx
+++ Phonebook/phonebook/src/App.jsx
@@ -4,19 +4,38 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  // Event handler for form submission
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = { name: newName };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  // Event handler for input field changes
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) => {
+          return <p key={person.name}>{person.name}</p>;
+        })}
+      </div>
     </div>
   );
 };
