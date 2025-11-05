e6548a2cbd8679cffa0e2133b58c19f1dd401f09
reagan luyinda <luyindareagan@gmail.com>
2025-11-05 14:10:26 +0300

    2.11 phonebook step 6,create data fetch from server

--- Phonebook/phonebook/src/App.jsx
+++ Phonebook/phonebook/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./Components/filter.jsx";
 import Form from "./Components/form.jsx";
 import Persons from "./Components/content.jsx";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  // Fetch initial data from the server
+  useEffect(() => {
+    console.log("Effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("Promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   // Event handler for form submission
   const addPerson = (event) => {
