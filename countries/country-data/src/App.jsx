a970527fc4de32b35eaa5d7cd28982e373920ae6
reagan luyinda <luyindareagan@gmail.com>
2025-11-17 19:40:16 +0300

    Show filtered countries if below 10

--- countries/country-data/src/App.jsx
+++ countries/country-data/src/App.jsx
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
   const [value, setValue] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    console.log("loading countries...");
+
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((Response) => {
+        setCountries(Response.data);
+      });
+  }, []);
 
   const handleChange = (event) => {
     setValue(event.target.value);
     console.log(event.target.value);
   };
+
+  const filtered = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(value.toLowerCase())
+  );
+
   return (
     <div>
       Find countries <input value={value} onChange={handleChange} />
+      {value && (
+        <div>
+          {filtered.length > 10 ? (
+            <p>Too many matches, specify another filter</p>
+          ) : (
+            filtered.map((country) => (
+              <li key={country.cca3}>{country.name.common}</li>
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
 };
